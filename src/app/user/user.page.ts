e9fcb02a30d936df973e88d24124d6791139ae67
David Rubio <dav.rubio@duocuc.cl>
2023-09-01 18:25:49 -0400

    configuraciones generales

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserModel } from '../models/UserModels';
 
 @Component({
@@ -16,7 +16,7 @@ export class UserPage implements OnInit {
 
   userInfoReceived: UserModel | undefined;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(private router: Router) {
     this.userInfoReceived = this.router.getCurrentNavigation()?.extras.state?.['user'];
   }
 
