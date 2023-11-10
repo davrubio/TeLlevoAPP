c45a4f04e70718b624e5b2b2c86568be2abbcbee
David Rubio <dav.rubio@duocuc.cl>
2023-11-10 18:19:20 -0300

    design and darkmode

--- src/app/components/base/header/header.component.ts
+++ src/app/components/base/header/header.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { UserLocalData } from 'src/app/models/user/user.info';
@@ -13,12 +14,13 @@ import { ManageSession } from 'src/app/utils/manage.session';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class HeaderComponent extends ManageSession implements OnInit {
 
   @Input({required:true})
   titlePage!: string;
+  darkMode = false;
 
   userData: UserLocalData;
 
@@ -31,7 +33,33 @@ export class HeaderComponent extends ManageSession implements OnInit {
     this.userData = ManageLocalData.getLocalData();
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.checkAppMode();
+  }
+
+  checkAppMode(){
+    const checkIsDarkMode = localStorage.getItem('darkModeActivated');
+    checkIsDarkMode == 'true' ? (this.darkMode = true) : (this.darkMode = false);
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+  /* initializeDarkTheme(isDark: any) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  } */
+
+  toggleChange() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark', this.darkMode);
+    if(this.darkMode){
+      localStorage.setItem('darkModeActivated', 'true');
+    } else {
+      localStorage.setItem('darkModeActivated', 'false');
+    }
+  }
+
+  /* toggleDarkTheme(shouldAdd: any) {
+    document.body.classList.toggle('dark', shouldAdd);
+  } */
 
   redirecToProfile(){
     this.router.navigate(['/profile'], {state: {user: this.userData}});
